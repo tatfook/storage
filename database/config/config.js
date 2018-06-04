c75dd334dbb2365b284889f82cecd54e1bce039f
wxaxiaoyao <765485868@qq.com>
2018-06-04 17:49:47 +0800

    add migrate secret config

--- database/config/config.js
+++ database/config/config.js
@@ -1,4 +1,9 @@
 const Sequelize = require("sequelize");
+const config = require("../../server/.config.js");
+
+const developmentConfig = config.development.database;
+const productionConfig = config.production.database;
+const testConfig = config.test.database;
 
 module.exports = {
   "development": {
@@ -6,28 +11,30 @@ module.exports = {
     //"password": "wuxiangan",
     //"database": "keepwork",
     //"host": "39.106.11.114",
-	"username": "root",
-	"password": "123456",
-	"database": "keepwork",
-	"host": "10.28.18.16",
-	"port": 23306,
+    "username": developmentConfig.username,
+    "password": developmentConfig.password,
+    "host": developmentConfig.host,
+	"port": developmentConfig.port,
+	"database": "keepwork-dev",
     "dialect": "mysql",
 	"operatorsAliases":Sequelize.Op
   },
   "test": {
-    "username": "root",
-	"password": "123456",
-    "database": "keepwork",
-    "host": "10.28.18.16",
-	"port": 23306,
+    "username": testConfig.username,
+    "password": testConfig.password,
+    "host": testConfig.host,
+	"port": testConfig.port,
+    "database": "keepwork-test",
     "dialect": "mysql"
+	"operatorsAliases":Sequelize.Op
   },
   "production": {
-    "username": "root",
-	"password": "123456",
+    "username": productionConfig.username,
+    "password": productionConfig.password,
+    "host": productionConfig.host,
+	"port": productionConfig.port || 3306,
     "database": "keepwork",
-    "host": "10.28.18.16",
-	"port": 23306,
     "dialect": "mysql"
+	"operatorsAliases":Sequelize.Op
   }
 };
