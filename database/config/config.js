4bff827584f5f58cadbe27c3ca757c0a185fd47e
wxaxiaoyao <765485868@qq.com>
2018-07-19 18:45:58 +0800

    add en config

--- database/config/config.js
+++ database/config/config.js
@@ -5,24 +5,25 @@ const configs = config.configs;
 const developmentConfig = configs.development.database;
 const releaseConfig = configs.release.database;
 const productionConfig = configs.production.database;
+const productionEnConfig = configs["production-en"].database;
 const testConfig = configs.test.database;
 
 module.exports = {
-  "development": {
-    "username": developmentConfig.username,
-    "password": developmentConfig.password,
-    "host": developmentConfig.host,
-	"port": developmentConfig.port,
-	"database": "keepwork-dev",
-    "dialect": "mysql",
-	"operatorsAliases":Sequelize.Op,
-  },
   "test": {
     "username": testConfig.username,
     "password": testConfig.password,
     "host": testConfig.host,
 	"port": testConfig.port,
     "database": "keepwork-test",
+    "dialect": "mysql",
+	"operatorsAliases":Sequelize.Op,
+  },
+  "development": {
+    "username": developmentConfig.username,
+    "password": developmentConfig.password,
+    "host": developmentConfig.host,
+	"port": developmentConfig.port,
+	"database": "keepwork-dev",
     "dialect": "mysql",
 	"operatorsAliases":Sequelize.Op,
   },
@@ -43,5 +44,33 @@ module.exports = {
     "database": "keepwork",
     "dialect": "mysql",
 	"operatorsAliases":Sequelize.Op,
-  }
+  },
+  "development-en": {
+    "username": developmentConfig.username,
+    "password": developmentConfig.password,
+    "host": developmentConfig.host,
+	"port": developmentConfig.port,
+	"database": "keepwork-dev-en",
+    "dialect": "mysql",
+	"operatorsAliases":Sequelize.Op,
+  },
+  "release-en": {
+    "username": releaseConfig.username,
+    "password": releaseConfig.password,
+    "host": releaseConfig.host,
+	"port": releaseConfig.port,
+    "database": "keepwork-rls-en",
+    "dialect": "mysql",
+	"operatorsAliases":Sequelize.Op,
+  },
+  "production-en": {
+    "username": productionEnConfig.username,
+    "password": productionEnConfig.password,
+    "host": productionEnConfig.host,
+	"port": productionEnConfig.port || 3306,
+    "database": "keepwork-en",
+    "dialect": "mysql",
+	"operatorsAliases":Sequelize.Op,
+  },
 };
+//console.log(developmentConfig);
