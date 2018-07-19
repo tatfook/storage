ceafdb5a7605ae2672421cb24312d8db05cf14d9
wxaxiaoyao <765485868@qq.com>
2018-07-19 18:21:30 +0800

    add en config

--- server/config.js
+++ server/config.js
@@ -46,16 +46,34 @@ const productionConfig = {
 	}
 }
 
+const productionEnConfig = {
+	//keepworkBaseURL: "http://keepwork.com/api/wiki/models/",
+	keepworkBaseURL: "http://10.28.18.6:8000/api/wiki/models/",
+	origin: "http://api-en.keepwork.com",
+	qiniu: {
+		bucketName: "keepwork",
+		bucketDomian: "http://ov62qege8.bkt.clouddn.com",
+	},
+	database: {
+		database:"keepwork-en",
+	}
+}
+
 const releaseConfig = {
 	origin: "http://api-release.keepwork.com",
 	//keepworkBaseURL: "http://release.keepwork.com/api/wiki/models/",
 	keepworkBaseURL: "http://10.28.18.2:8088/api/wiki/models/",
-	qiniu: {
-		bucketName: "keepwork-dev",
-		bucketDomian: "http://oy41aju0m.bkt.clouddn.com",
-	},
 	database: {
 		database:"keepwork-rls",
+	}
+}
+
+const releaseEnConfig = {
+	origin: "http://api-release-en.keepwork.com",
+	//keepworkBaseURL: "http://release.keepwork.com/api/wiki/models/",
+	keepworkBaseURL: "http://10.28.18.2:8088/api/wiki/models/",
+	database: {
+		database:"keepwork-rls-en",
 	}
 }
 
@@ -63,12 +81,17 @@ const developmentConfig = {
 	origin: "http://api-stage.keepwork.com",
 	//keepworkBaseURL: "http://stage.keepwork.com/api/wiki/models/",
 	keepworkBaseURL: "http://10.28.18.2:8900/api/wiki/models/",
-	qiniu: {
-		bucketName: "keepwork-dev",
-		bucketDomian: "http://oy41aju0m.bkt.clouddn.com",
-	},
 	database: {
 		database:"keepwork-dev",
+	}
+}
+
+const developmentEnConfig = {
+	origin: "http://api-stage-en.keepwork.com",
+	//keepworkBaseURL: "http://stage.keepwork.com/api/wiki/models/",
+	keepworkBaseURL: "http://10.28.18.2:8900/api/wiki/models/",
+	database: {
+		database:"keepwork-dev-en",
 	}
 }
 
@@ -80,8 +103,11 @@ const localConfig = {
 
 const configs = {
 	"production": _.merge({}, defaultConfig, productionConfig, secretConfig),
+	"production-en": _.merge({}, defaultConfig, productionEnConfig, secretConfig),
 	"release": _.merge({}, defaultConfig, releaseConfig, secretConfig),
+	"release-en": _.merge({}, defaultConfig, releaseEnConfig, secretConfig),
 	"development": _.merge({}, defaultConfig, developmentConfig, secretConfig),
+	"development-en": _.merge({}, defaultConfig, developmentEnConfig, secretConfig),
 	"test": _.merge({}, defaultConfig, testConfig, secretConfig),
 	"local": _.merge({}, defaultConfig, localConfig, secretConfig),
 }
