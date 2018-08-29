40e56550bd75784b388c7acd0c8d0c914c0df274
wxaxiaoyao <765485868@qq.com>
2018-08-29 16:58:08 +0800

    add log

--- config/config.default.js
+++ config/config.default.js
@@ -15,3 +15,15 @@ exports.security = {
 		enable: false,
 	},
 }
+
+exports.onerror = {
+	all: (e, ctx) => {
+		const message = e.stack || e.message || e.toString();
+
+		ctx.status = e.status || 500;
+		ctx.body = message;
+		if (e.name == "SequelizeUniqueConstraintError") {
+			ctx.status = 409;
+		}
+	}
+}
