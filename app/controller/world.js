9d0949017ccd3e3e686338610aa4aa5b29099196
onedou <onedou@126.com>
2018-10-17 14:23:01 +0800

    update world api

--- app/controller/world.js
+++ app/controller/world.js
@@ -1,5 +1,6 @@
 const joi = require('joi');
 const _ = require('lodash');
+const base32 = require('base32');
 
 const Controller = require('../core/controller.js');
 const {
@@ -14,18 +15,108 @@ const World = class extends Controller {
   }
 
   async test() {
-    return this.generateDefaultWorld();
+    return this.generateDefaultWorld('你好啊');
   }
 
-  async generateDefaultWorld() {
-		
+  async generateDefaultWorld(worldName) {
+    let userInfo = this.authenticated()
 
-    console.log(this.app.git.getTree())
+    if (!userInfo || !userInfo.username) {
+      return false
+    }
 
-		// console.log(gitGatewayApi)
-		// console.log(paracraftDefaultProject)
+    let tree = await this.app.git.getTree()
+    let baseWorldName = this.base32(worldName)
 
+    await new Promise((resolve, reject) => {
+      let index = 0;
+
+      if (!tree || tree.length == 0) {
+        resolve()
+      }
+
+      _.forEach(tree, async (item, key) => {
+        if (item && item.path) {
+
+          let content = await this.app.git.getContent(null, item.path);
+
+          item.content = content;
+          index++
+
+          if (index == tree.length) {
+            resolve()
+          }
+        }
+      })
+    })
+
+    let result = await this.app.git.createProject(userInfo.username, baseWorldName)
+    
+    // if (!result) {
+    //   return this.ctx.throw(500);
+    // }
+
+    let worldProject = (userInfo.username || '') + '/' + (worldName || '')
+  
+    _.forEach(tree, (item, key) => {
+      if (item && item.path && item.content) {
+        if (item.path == 'tag.xml') {
+          item.content = item.content.replace('name="DefaultName"', `name="${worldName || ''}"`)
+        }
+
+        this.app.git.writeFile(worldProject, item.path, item.content)
+      }
+    })
+
+    console.log(this.ctx.state.token)
+    console.log(this.ctx)
+    // console.log(tree);
     // return this.ctx.throw(500);
+  }
+
+  // =转成-equal  +转成-plus  /转成-slash
+  base32(text) {
+    if (text) {
+      let notLetter = text.match(/[^a-zA-Z]/g);
+
+      if (notLetter) {
+        text = base32.encode(text);
+
+        text = text.replace(/[=]/g, '-equal');
+        text = text.replace(/[+]/g, '-plus');
+        text = text.replace(/[/]/g, '-slash');
+
+        text = 'world_base32_' + text;
+      } else {
+        text = 'world_' + text;
+      }
+
+      return text;
+    } else {
+      return nil;
+    }
+  }
+
+  unbase32(text) {
+    if (text) {
+      let notLetter = text.match('world_base32_');
+
+      if (notLetter) {
+        text = text.replace('world_base32_', '');
+
+        text = text.replace(/-equal/g, '=');
+        text = text.replace(/-plus/g, '+');
+        text = text.replace(/-slash/g, '/');
+
+        return Encoding.from_base32(text);
+      } else {
+        text = text.replace('world_', '');
+
+        return text;
+      }
+    } else {
+      return nil;
+    }
   }
 };
 
--- app/core/git.js
+++ app/core/git.js
@@ -1,53 +1,190 @@
 
 
-
-class Git{
+class Git {
   constructor(config, app) {
-		this.config = config;
+    this.config = config;
     this.app = app;
-    this.isConfigRight = true
+    this.isConfigRight = true;
+    this.cacheTree = [];
+    this.cacheContent = {};
 
-    if (!this.app ||
-			!this.app.config ||
-			!this.app.config.self) {
-				this.isConfigRight = false;
-			}
+    if (
+      !this.app ||
+      !this.app.config ||
+      !this.app.config.self ||
+      !this.app.config.self.secret ||
+      !this.app.util ||
+      !this.app.util.jwt_encode
+    ) {
+      this.isConfigRight = false;
+    }
 
     this.gitGatewayApi = this.app.config.self.gitGatewayURL || '';
-		this.paracraftDefaultProject = this.app.config.self.paracraftDefaultProject || '';
+    this.paracraftDefaultProject =
+      this.app.config.self.paracraftDefaultProject || '';
 
     if (!this.gitGatewayApi || !this.paracraftDefaultProject) {
       this.isConfigRight = false;
-		}
-		
-		if (!this.app || !this.app.axios) {
-			this.isConfigRight = false;
+    }
+
+    if (!this.app || !this.app.axios) {
+      this.isConfigRight = false;
     }
   }
-  
-  getTree() {
+
+  async getAdminToken() {
     if (!this.isConfigRight) {
-      return []
+      return false;
     }
 
+    if (!this.adminToken) {
+      this.adminToken = this.app.util.jwt_encode({userId:1, roleId: 10}, this.app.config.self.secret, 3600 * 24 * 365 * 10)
+    }
+
+    return this.adminToken
+  }
+
+  async createProject(userName, worldName) {
+    if (!this.isConfigRight || typeof userName != 'string' || typeof worldName != 'string') {
+      return false;
+    }
+
+    let url = this.gitGatewayApi + '/projects/user/' + userName;
+    let adminToken = await this.getAdminToken()
+
+    let axios = this.app.axios.create({
+      headers: {
+        Authorization: "Bearer " + adminToken || '',
+        "Content-Type": "application/json"
+      }
+    })
+
+    try {
+      let response = await axios.post(url, {
+        sitename: worldName,
+        visibility: 'public'
+      });
+      
+      if (response && response.data && response.data.created) {
+        return true;
+      } else {
+        return false;
+      }
+    } catch (error) {
+      return false;
+    }
+  }
+
+  async removeProject() {
+    if (!this.isConfigRight) {
+      return false;
+    }
+
+    let adminToken = this.app.util.jwt_encode;
+  }
+
+  async getTree(projectPath) {
+    if (!this.isConfigRight) {
+      return [];
+    }
+
+    projectPath = projectPath || this.paracraftDefaultProject;
+
+    if (projectPath == this.paracraftDefaultProject) {
+      if (Array.isArray(this.cacheTree) && this.cacheTree.length > 0) {
+        return this.cacheTree
+      }
+    }
+
+    let url =
+      this.gitGatewayApi +
+      '/projects/' +
+      encodeURIComponent(projectPath) +
+      '/tree/?recursive';
+    let response;
+
+    try {
+      response = await this.app.axios.get(url);
+    } catch (error) {}
+
+    if (response && response.data) {
+      this.cacheTree = response.data;
+
+      return response.data;
+    } else {
+      return [];
+    }
+  }
+
+  async getContent(projectPath, path) {
+    if (!this.isConfigRight || !path) {
+      return '';
+    }
+
+    projectPath = projectPath || this.paracraftDefaultProject;
+
+    let url =
+      this.gitGatewayApi +
+      '/projects/' +
+      encodeURIComponent(projectPath) +
+      '/files/' +
+      encodeURIComponent(path);
+
+    if (projectPath == this.paracraftDefaultProject) {
+      if (this.cacheContent[url]) {
+        return this.cacheContent[url]
+      }
+    }
+
+    let response;
+
+    try {
+      response = await this.app.axios.get(url);
+    } catch (error) {}
+
+    if (response && response.data && response.data.content) {
+      this.cacheContent[url] = response.data.content;
+
+      return response.data.content;
+    } else {
+      return '';
+    }
+  }
+
+  writeFile(projectPath, path, content) {
+    if (!this.isConfigRight || !projectPath || !path) {
+      return false;
+    }
+
+    if (projectPath == this.paracraftDefaultProject) {
+      return false;
+    }
+
+    let url =
+      this.gitGatewayApi +
+      '/projects/' +
+      encodeURIComponent(projectPath) +
+      '/files/' +
+      encodeURIComponent(path);
     
   }
 
-  getContent() {
+  deleteFile(projectPath, path) {
+    if (!this.isConfigRight) {
+      return false;
+    }
 
-  }
-
-  writeFile() {
-
-  }
-
-  deleteFile() {
-    
+    let url =
+      this.gitGatewayApi +
+      '/projects/' +
+      encodeURIComponent(projectPath) +
+      '/files/' +
+      encodeURIComponent(path);
   }
 }
 
 module.exports = app => {
   const config = app.config.self;
 
-  app.git = new Git(config, app)
-}
+  app.git = new Git(config, app);
+};
