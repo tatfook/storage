f99c872abc7a46b5c696af74516a9896bc02d1d9
wxaxiaoyao <765485868@qq.com>
2018-10-30 17:54:49 +0800

    fix bug

--- app/controller/issue.js
+++ app/controller/issue.js
@@ -67,6 +67,7 @@ const Issue = class extends Controller {
 		const issue = await this.model.issues.getById(id, userId);
 		if (!issue) this.throw(400);
 		issue.assigns = await this.model.issues.getIssueAssigns(issue.assigns);
+		issue.user = await this.model.users.getById(issue.userId);
 
 		return this.success(issue);
 	}
